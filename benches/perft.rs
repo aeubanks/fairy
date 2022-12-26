c7745927c85e0b9a7a6e44cd5092320fb991ee8b
Arthur Eubanks <ayueubanks@gmail.com>
2022-12-26 14:36:41 -0700

    Add normal perft (filtering out traditionally illegal moves) benches

--- benches/perft.rs
+++ benches/perft.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use fairy::board::Presets;
-use fairy::perft::{fen, perft_all, Position};
+use fairy::perft::{fen, perft, perft_all, Position};
 use fairy::player::Player::*;
 
 fn run_perft_all(c: &mut Criterion) {
@@ -10,8 +10,18 @@ fn run_perft_all(c: &mut Criterion) {
         player: White,
     };
     let pos2 = fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-    c.bench_function("classical1", |b| b.iter(|| perft_all(black_box(&pos1), 5)));
-    c.bench_function("classical2", |b| b.iter(|| perft_all(black_box(&pos2), 4)));
+    c.bench_function("perft_classical1", |b| {
+        b.iter(|| perft(black_box(&pos1), 5))
+    });
+    c.bench_function("perft_classical2", |b| {
+        b.iter(|| perft(black_box(&pos2), 4))
+    });
+    c.bench_function("perft_all_classical1", |b| {
+        b.iter(|| perft_all(black_box(&pos1), 5))
+    });
+    c.bench_function("perft_all_classical2", |b| {
+        b.iter(|| perft_all(black_box(&pos2), 4))
+    });
 }
 
 criterion_group!(benches, run_perft_all);
